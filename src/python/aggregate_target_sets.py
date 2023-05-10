61d509abdee3bd4cc442eee38d24a43bae9ec94c
buriedsand <brandonlukas@gmail.com>
2023-05-10 13:15:45 -0500

    empty file

--- src/python/aggregate_target_sets.py
+++ src/python/aggregate_target_sets.py
@@ -7,8 +7,12 @@ with open(output_file, "w") as output:
     for input_file in input_files:
         tf_name = input_file.split("/")[-1].split(".")[0]
         with open(input_file, "r") as f:
-            # Skip header line
-            next(f)
+            try:
+                # Skip header line
+                next(f)
+            except StopIteration:
+                # Empty file, skip processing
+                continue
             for line in f:
                 target_gene = line.strip().split("\t")[0]
                 if target_gene:
